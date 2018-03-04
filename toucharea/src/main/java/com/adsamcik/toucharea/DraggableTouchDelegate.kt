1a60fa54adfa20f7ac1a6ed320a3e88f22a07983
adsamcik <adsamcik@users.noreply.github.com>
2018-03-04 11:43:17 +0100

    Refactoring, Add javadoc

--- toucharea/src/main/java/com/adsamcik/toucharea/DraggableTouchDelegate.kt
+++ toucharea/src/main/java/com/adsamcik/toucharea/DraggableTouchDelegate.kt
@@ -8,21 +8,23 @@ import android.view.View
  * Touch delegate that can handle dragging/translation
  */
 class DraggableTouchDelegate(private val mOffsetRect: Rect, override val view: View) : AbstractTouchDelegate() {
-    private val mHitRect = Rect()
+    /**
+     * If true all move events and up event are sent to the view
+     * If false no move event or up event is sent to view
+     * True or False value is set during down event
+     */
     private var mDelegateTargeted = false
 
-    private fun updateHitRect() {
-        //view.getHitRect(mOffsetRect)
-        val tX = view.x.toInt()
-        val tY = view.y.toInt()
-        mHitRect.left = tX - mOffsetRect.left
-        mHitRect.top = tY - mOffsetRect.top
-        mHitRect.right = tX + view.width + mOffsetRect.right
-        mHitRect.bottom = tY + view.height + mOffsetRect.bottom
-    }
+    private val hitRect: Rect
+        get() {
+            val tX = view.x.toInt()
+            val tY = view.y.toInt()
+            return Rect(tX - mOffsetRect.left, tY - mOffsetRect.top, tX + view.width + mOffsetRect.right, tY + view.height + mOffsetRect.bottom)
+        }
 
     /**
      * Updates offsets of the delegate
+     *
      */
     fun updateOffsets(left: Int, top: Int, right: Int, bottom: Int) {
         mOffsetRect.left = left
@@ -33,6 +35,8 @@ class DraggableTouchDelegate(private val mOffsetRect: Rect, override val view: V
 
     /**
      * Updates offsets of the delegate
+     *
+     * @param rect Rectangle of values copied for the delegate
      */
     fun updateOffsets(rect: Rect) {
         mOffsetRect.left = rect.left
@@ -54,8 +58,7 @@ class DraggableTouchDelegate(private val mOffsetRect: Rect, override val view: V
                 }
             }
             MotionEvent.ACTION_DOWN -> {
-                updateHitRect()
-                if (mHitRect.contains(event.x.toInt(), event.y.toInt())) {
+                if (hitRect.contains(event.x.toInt(), event.y.toInt())) {
                     mDelegateTargeted = true
                     sendToDelegate = true
                 }
